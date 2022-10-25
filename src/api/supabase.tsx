2e81716a3261e0015736fa71c9542ca52640aa2b
Stephan Grobler <stephan.grobler@gmail.com>
2022-10-25 11:58:46 +0200

    add user sign up and login

--- src/api/supabase.tsx
+++ src/api/supabase.tsx
@@ -4,13 +4,24 @@ import { createClient } from '@supabase/supabase-js';
 // Create a single supabase client for interacting with your database
 
 export class Supabase {
-
+    
     supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
     public supabase = createClient(
         'https://qaujdkgbydkyuvxlyaqu.supabase.co',
         this.supabaseKey!
-    );
-
+        );
+        
+    async signUp(arg0: { email: string; password: string; }) {
+        return await this.supabase.auth.signUp({email: arg0.email, password: arg0.password});
+    }
+    
+    async signInWithPassword(arg0: { email: string; password: string; }): Promise<any> {
+        return await this.supabase.auth.signIn({
+            email: arg0.email,
+            password: arg0.password,
+        });
+    }    
+    
     async getPlayers() {
         const { data, error } = await this.supabase.from('players').select(`
         *,
---
+++ src/components/AppTabsComponent.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getCurrentInnings, getCurrentMatch, persistInnings, persistMatch } from '../api/matchData';
+import { useAuth } from '../contexts/AuthProvider';
+import MatchComponent from './MatchComponent';
+import ScoreCardTabComponent from './ScoreCardTabComponent';
+import ScoringTabComponent from './ScoringTabComponent';
+
+const AppTabsComponent = () => {
+    const { onLogout } = useAuth();   
+
+    const matchData = getCurrentMatch();
+    const inningData = getCurrentInnings(matchData.id);
+
+    const [match, setMatch] = useState(matchData);
+    const [innings, setInnings] = useState(inningData);
+
+    const [team1, setTeam1] = useState('');
+    const [team2, setTeam2] = useState('');
+    const [activeTab, setActiveTab] = useState('Scoring');
+
+    const handleEndMatch = () => {
+        handleEndInnings();
+
+        localStorage.removeItem('currentMatch');
+        localStorage.removeItem('currentInnings');
+
+        const newMatch = getCurrentMatch();
+        const newInnings = getCurrentInnings(newMatch.id);
+        setMatch(newMatch);
+        setInnings(newInnings);
+    };
+
+    const handleEndInnings = () => {
+        const matchData = { ...match, innings: { ...match.innings, [innings.id]: innings } };
+        setMatch(matchData);
+        persistMatch(matchData);
+        localStorage.removeItem('currentInnings');
+        localStorage.removeItem('currentOver');
+        localStorage.removeItem('currentBowler');
+        localStorage.removeItem('currentBatsmen');
+
+        const inningData = getCurrentInnings(match.id);
+        persistInnings(inningData);
+        setInnings(inningData);
+    };
+
+    return (
+        <div className="container mx-auto h-screen">
+            <div className="tabs tabs-boxed">
+                <div
+                    className={activeTab === 'MatchSettings' ? 'tab tab-active flex-1' : 'tab flex-1'}
+                    onClick={() => setActiveTab('MatchSettings')}>
+                    Match Settings
+                </div>
+                <div
+                    className={activeTab === 'Scoring' ? 'tab tab-active flex-1' : 'tab flex-1'}
+                    onClick={() => setActiveTab('Scoring')}>
+                    Scoring
+                </div>
+                <div
+                    className={activeTab === 'ScoreCard' ? 'tab tab-active flex-1' : 'tab flex-1'}
+                    onClick={() => setActiveTab('ScoreCard')}>
+                    Score Cards
+                </div>
+                <div className="flex-1">
+                    <button className="btn btn-small" onClick={onLogout}>
+                        Log Out
+                    </button>
+                </div>
+            </div>
+
+            {activeTab === 'MatchSettings' && (
+                <MatchComponent
+                    team1={team1}
+                    team2={team2}
+                    setTeam1={setTeam1}
+                    setTeam2={setTeam2}
+                    handleEndInnings={handleEndInnings}
+                    handleEndMatch={handleEndMatch}
+                />
+            )}
+            {activeTab === 'Scoring' && (
+                <ScoringTabComponent match={match} setMatch={setMatch} innings={innings} setInnings={setInnings} />
+            )}
+
+            {activeTab === 'ScoreCard' && <ScoreCardTabComponent />}
+        </div>
+    );
+};
+
+export default AppTabsComponent;
